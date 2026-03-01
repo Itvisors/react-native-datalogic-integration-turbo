d2fbed14b14d5e218dbf453ad17970f39b5170cc
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2026-03-01 07:21:59 +0100

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,8 +1,10 @@
 // Force null for ios. This ensures for cross platform apps nothing is included for this module
+// Explicitly include android so the android build picks up the dependency
 module.exports = {
   dependency: {
     platforms: {
       ios: null,
+      android: {},
     },
   },
 };
