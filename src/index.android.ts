3992aa2079153d19638a93a31ab1339385bb6caf
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2026-03-07 06:58:57 +0100

    Remove boilerplate leftover

--- src/index.android.ts
+++ src/index.android.ts
@@ -1,12 +1,5 @@
 import DatalogicIntegrationTurbo from './NativeDatalogicIntegrationTurbo';
 
-export function multiply(a: number, b: number): number {
-  if (!DatalogicIntegrationTurbo) {
-    throw new Error('Native library DatalogicIntegrationTurbo not available');
-  }
-  return DatalogicIntegrationTurbo.multiply(a, b);
-}
-
 export function startReadListener(): void {
   if (!DatalogicIntegrationTurbo) {
     throw new Error('Native library DatalogicIntegrationTurbo not available');
